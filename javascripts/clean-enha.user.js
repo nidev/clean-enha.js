8808928818a710bc1e263330e6f9901fba8d8ab2
Nidev Plontra <nidev.plontra@gmail.com>
2015-07-16 22:35:05 +0900

    Version bump. Changes from 7e3ef90 applied.

--- javascripts/clean-enha.user.js
+++ javascripts/clean-enha.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id            nidev
 // @name          CleanEnhaJS
-// @version       20150715
+// @version       20150716
 // @namespace     https://g.nidev.org/clean-enha.js/
 // @author        Nidev
 // @homepage      http://g.nidev.org/clean-enha.js
@@ -18,4 +18,4 @@
 // @run-at        document-end
 // ==/UserScript==
 
-(function(){var s=document.getElementsByTagName('del');for(var i=0;i<s.length;i++){s[i].style.cssText='display:none;'};var engine;var a;if(document.querySelector("#_core")){a=document.querySelector("article.namu-doc").getElementsByTagName("a");engine="namumirror"}else if(document.querySelector(".wiki-content")){a=document.querySelector(".wiki-content").getElementsByTagName("a");engine="namu"}else{a=document.getElementsByTagName('a');engine="enha"}if(!a)return;switch(engine){case"namumirror":for(var i=0;i<a.length;i++){if(a[i].getAttribute("class")!=="wiki-fn"&&a[i].href.indexOf('#')==-1){var title=decodeURI(a[i].pathname.substring(6,a[i].pathname.length));if(title!==a[i].innerHTML){a[i].innerHTML+='<sup>'+title+'</sup>'}}}break;case"namu":for(var i=0;i<a.length;i++){if(a[i].title&&a[i].getAttribute("class")==="wiki-link-internal"&&a[i].title!==a[i].innerHTML){a[i].innerHTML+='<sup>'+a[i].title+'</sup>'}}break;default:for(var i=0;i<a.length;i++){if(!a[i].id&&a[i].title&&a[i].title!==a[i].innerHTML){a[i].innerHTML+='<sup>'+a[i].title+'</sup>'}}}})();
+var clean_enhajs=(function(){"use strict";var i,s,engine,a,title;s=document.getElementsByTagName('del');for(i=0;i<s.length;i+=1){s[i].style.cssText='display:none;'}if(document.querySelector("#_core")){a=document.querySelector("article.namu-doc").getElementsByTagName("a");engine="namumirror"}else if(document.querySelector(".wiki-content")){a=document.querySelector(".wiki-content").getElementsByTagName("a");engine="namu"}else{a=document.getElementsByTagName('a');engine="enha"}if(!a){return}switch(engine){case"namumirror":for(i=0;i<a.length;i+=1){if(a[i].getAttribute("class")===null&&a[i].href.indexOf('#')===-1){title=decodeURI(a[i].pathname.substring(6,a[i].pathname.length));if(title!==a[i].innerHTML){a[i].innerHTML+='<sup>'+title+'</sup>'}}}break;case"namu":for(i=0;i<a.length;i+=1){if(a[i].title&&a[i].getAttribute("class")==="wiki-link-internal"&&a[i].title!==a[i].innerHTML){a[i].innerHTML+='<sup>'+a[i].title+'</sup>'}}break;default:for(i=0;i<a.length;i+=1){if(!a[i].id&&a[i].title&&a[i].title!==a[i].innerHTML){a[i].innerHTML+='<sup>'+a[i].title+'</sup>'}}}}());
